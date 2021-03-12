c56711659f35ef46136e4e5805107003ec61fcb8
Philippe Hürlimann <p@hurlimann.org>
2021-03-12 02:05:31 +0100

    implement sending of tweet

--- vactracher.py
+++ vactracher.py
@@ -1,27 +1,25 @@
 import os
+import toml
+from collections import namedtuple
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+import tweepy
 
-DEFAULT_STATE_FILE = '/tmp/vacstate.txt'
+DEFAULT_STATE_FILE = "/tmp/vacstate.txt"
 
-
-class Info:
-    def __init__(self, n, date, source):
-        self.n = n
-        self.date = date
-        self.source = source
+Infos = namedtuple("Infos", "delivered administered fully_vaccinated")
+Info = namedtuple("Info", "n date source")
 
 
 def extract_description_info(description):
-    d = dict([
-        tuple([x.strip() for x in kv.split(':')])
-        for kv in description.split('–')
-    ])
+    d = dict(
+        [tuple([x.strip() for x in kv.split(":")]) for kv in description.split("–")]
+    )
 
-    date = datetime.strptime(d['Status'], '%d.%m.%Y, %H.%Mh')
+    date = datetime.strptime(d["Status"], "%d.%m.%Y, %H.%Mh")
 
-    return date, d['Source']
+    return date, d["Source"]
 
 
 def extract_info(soup, string):
@@ -32,26 +30,32 @@ def extract_info(soup, string):
     date, source = extract_description_info(description)
 
     row = table_head.parent
-    n = int(row.find_next_sibling().text.replace(' ', ''))
+    n = int(row.find_next_sibling().text.replace(" ", ""))
 
     return Info(n, date, source)
 
 
 def retrieve_info():
-    r = requests.get('https://www.covid19.admin.ch/en/overview')
+    r = requests.get("https://www.covid19.admin.ch/en/overview")
 
-    s = BeautifulSoup(r.text, features='html.parser')
+    s = BeautifulSoup(r.text, features="html.parser")
 
-    delivered = extract_info(s, 'Vaccine doses delivered to cantons and FL')
-    administered = extract_info(s, 'Administered vaccine doses')
-    fully_vaccinated = extract_info(s, 'Fully vaccinated people')
+    delivered = extract_info(s, "Vaccine doses delivered to cantons and FL")
+    administered = extract_info(s, "Administered vaccine doses")
+    fully_vaccinated = extract_info(s, "Fully vaccinated people")
 
-    return delivered, administered, fully_vaccinated
+    return Infos(delivered, administered, fully_vaccinated)
 
 
-class App():
-    def __init__(self, state_file):
+class App:
+    def __init__(
+        self, state_file, api_key, api_secret, access_token, access_token_secret
+    ):
         self.state_file = state_file
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
 
     def read_date_last_tweet(self):
         try:
@@ -62,22 +66,42 @@ class App():
         except IOError:
             return datetime.fromtimestamp(0)
 
+    def send_tweet(self, infos):
+        auth = tweepy.OAuthHandler(self.api_key, self.api_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        api = tweepy.API(auth)
+        api.update_status(
+            f"Delivered vaccines: {infos.delivered.n}\n"
+            f"Administered doses: {infos.administered.n}\n"
+            f"Fully vaccinated people: {infos.fully_vaccinated.n}"
+        )
+
 
 def main():
-    state_file = os.environ.get('STATE_FILE', DEFAULT_STATE_FILE)
-    app = App(state_file)
+    d = toml.load("secrets.toml")
+
+    state_file = os.environ.get("STATE_FILE", DEFAULT_STATE_FILE)
+
+    api_key = d["api_key"]
+    api_secret = d["api_secret"]
+    access_token = d["access_token"]
+    access_token_secret = d["access_token_secret"]
+    app = App(state_file, api_key, api_secret, access_token, access_token_secret)
 
     date_last_tweet = app.read_date_last_tweet()
 
-    delivered, administered, fully_vaccinated = retrieve_info()
-    data_date = max(max(delivered.date, administered.date),
-                    fully_vaccinated.date)
+    infos = retrieve_info()
+    data_date = max(
+        max(infos.delivered.date, infos.administered.date), infos.fully_vaccinated.date
+    )
 
     if date_last_tweet < data_date:
-        with open(state_file, 'w') as f:
+        app.send_tweet(infos)
+
+        with open(state_file, "w") as f:
             st = 1 + data_date.timestamp()  # avoid rounding errors
-            f.write(f'{st:0.0f}')
+            f.write(f"{st:0.0f}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

3adfd43841915b9d1d2fcd9650a59a72933d285c
Philippe Hürlimann <p@hurlimann.org>
2021-03-25 00:35:51 +0100

    Added nixos module

---
+++ nixos/vactracher.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, lib, ... }:
+let cfg = config.services.vactracher;
+
+in {
+  options.services.vactracher = with lib; {
+    enable = mkEnableOption
+      "VactraCHer, the Free and Open Source media streaming server";
+
+    user = mkOption {
+      default = "vactracher";
+      type = types.str;
+      description = "User account under which vactracher runs.";
+    };
+
+    dataDir = mkOption {
+      default = "/var/lib/${cfg.user}";
+      type = types.str;
+      description = ''
+        Home directory of the VactraCHer user and where its data will be written
+        to.
+      '';
+    };
+
+    envScript = mkOption {
+      type = types.str;
+      description = "Script to source before starting app";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.vactracher = {
+      description = "VactraCHer Tweeter";
+      after = [ "network.target" ];
+      requires = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      startAt = "hourly";
+
+      environment.STATE_FILE = "${cfg.dataDir}/state_file";
+
+      serviceConfig = {
+        script = with lib; ''
+          source ${cfg.envScript}
+          ${pkgs.vactracher}/bin/vactracher
+        '';
+        User = cfg.user;
+      };
+    };
+
+    users.users.vactracher = {
+      description = "VactraCHer service user";
+      name = cfg.user;
+      home = cfg.dataDir;
+      createHome = true;
+      isSystemUser = true;
+      extraGroups = cfg.extraUserGroups;
+    };
+  };
+}
